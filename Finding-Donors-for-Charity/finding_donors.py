fd94113ce9131bbb0440d62b66946ab8d6b53349
Vishal Vaishnav <thinkdifferent.vshal@gmail.com>
2017-02-02 15:38:09 +0530

    Initial Code for finding_donors

---
+++ Finding-Donors-for-Charity/finding_donors.py
@@ -0,0 +1,271 @@
+
+# # Machine Learning Engineer Nanodegree
+# ## Supervised Learning
+# ## Project: Finding Donors for *CharityML*
+
+
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from time import time
+from IPython.display import display # Allows the use of display() for DataFrames
+
+# Import supplementary visualization code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+get_ipython().magic(u'matplotlib inline')
+
+# Load the Census dataset
+data = pd.read_csv("census.csv")
+
+# Success - Display the first record
+display(data.head(n=1))
+
+
+# TODO: Total number of records
+n_records = len(data)
+
+# TODO: Number of records where individual's income is more than $50,000
+n_greater_50k = 0
+for entry in data.income:
+    if entry == '>50K':
+        n_greater_50k = n_greater_50k+1
+
+
+# TODO: Number of records where individual's income is at most $50,000
+n_at_most_50k = 0
+
+for entry in data.income:
+    if entry == '<=50K':
+        n_at_most_50k = n_at_most_50k + 1
+
+# TODO: Percentage of individuals whose income is more than $50,000
+greater_percent = (float(n_greater_50k)/n_records)*100
+
+# Print the results
+print "Total number of records: {}".format(n_records)
+print "Individuals making more than $50,000: {}".format(n_greater_50k)
+print "Individuals making at most $50,000: {}".format(n_at_most_50k)
+print "Percentage of individuals making more than $50,000: {:.2f}%".format(greater_percent)
+
+
+
+# Split the data into features and target label
+income_raw = data['income']
+features_raw = data.drop('income', axis = 1)
+
+
+# Visualize skewed continuous features of original data
+vs.distribution(data)
+
+
+# Log-transform the skewed features
+skewed = ['capital-gain', 'capital-loss']
+features_raw[skewed] = data[skewed].apply(lambda x: np.log(x + 1))
+
+# Visualize the new log distributions
+vs.distribution(features_raw, transformed = True)
+
+
+# Import sklearn.preprocessing.StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+# Initialize a scaler, then apply it to the features
+scaler = MinMaxScaler()
+numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+features_raw[numerical] = scaler.fit_transform(data[numerical])
+
+# Show an example of a record with scaling applied
+display(features_raw.head(n = 1))
+
+
+
+import pandas as pd
+# TODO: One-hot encode the 'features_raw' data using pandas.get_dummies()
+features = pd.get_dummies('features_raw')
+
+# TODO: Encode the 'income_raw' data to numerical values
+income = pd.get_dummies('income_raw')
+
+
+# Print the number of features after one-hot encoding
+encoded = list(features.columns)
+print "{} total features after one-hot encoding.".format(len(encoded))
+
+# Uncomment the following line to see the encoded feature names
+print encoded
+
+
+# Import train_test_split
+from sklearn.cross_validation import train_test_split
+
+# Split the 'features' and 'income' data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, income, test_size = 0.2, random_state = 0)
+
+# Show the results of the split
+print "Training set has {} samples.".format(X_train.shape[0])
+print "Testing set has {} samples.".format(X_test.shape[0])
+
+
+
+# TODO: Calculate accuracy
+accuracy = None
+
+# TODO: Calculate F-score using the formula above for beta = 0.5
+fscore = None
+
+# Print the results 
+print "Naive Predictor: [Accuracy score: {:.4f}, F-score: {:.4f}]".format(accuracy, fscore)
+
+
+
+
+# TODO: Import two metrics from sklearn - fbeta_score and accuracy_score
+
+def train_predict(learner, sample_size, X_train, y_train, X_test, y_test): 
+    '''
+    inputs:
+       - learner: the learning algorithm to be trained and predicted on
+       - sample_size: the size of samples (number) to be drawn from training set
+       - X_train: features training set
+       - y_train: income training set
+       - X_test: features testing set
+       - y_test: income testing set
+    '''
+    
+    results = {}
+    
+    # TODO: Fit the learner to the training data using slicing with 'sample_size'
+    start = time() # Get start time
+    learner = None
+    end = time() # Get end time
+    
+    # TODO: Calculate the training time
+    results['train_time'] = None
+        
+    # TODO: Get the predictions on the test set,
+    #       then get predictions on the first 300 training samples
+    start = time() # Get start time
+    predictions_test = None
+    predictions_train = None
+    end = time() # Get end time
+    
+    # TODO: Calculate the total prediction time
+    results['pred_time'] = None
+            
+    # TODO: Compute accuracy on the first 300 training samples
+    results['acc_train'] = None
+        
+    # TODO: Compute accuracy on test set
+    results['acc_test'] = None
+    
+    # TODO: Compute F-score on the the first 300 training samples
+    results['f_train'] = None
+        
+    # TODO: Compute F-score on the test set
+    results['f_test'] = None
+       
+    # Success
+    print "{} trained on {} samples.".format(learner.__class__.__name__, sample_size)
+        
+    # Return the results
+    return results
+
+
+# TODO: Import the three supervised learning models from sklearn
+
+# TODO: Initialize the three models
+clf_A = None
+clf_B = None
+clf_C = None
+
+# TODO: Calculate the number of samples for 1%, 10%, and 100% of the training data
+samples_1 = None
+samples_10 = None
+samples_100 = None
+
+# Collect results on the learners
+results = {}
+for clf in [clf_A, clf_B, clf_C]:
+    clf_name = clf.__class__.__name__
+    results[clf_name] = {}
+    for i, samples in enumerate([samples_1, samples_10, samples_100]):
+        results[clf_name][i] =         train_predict(clf, samples, X_train, y_train, X_test, y_test)
+
+# Run metrics visualization for the three supervised learning models chosen
+vs.evaluate(results, accuracy, fscore)
+
+
+
+# TODO: Import 'GridSearchCV', 'make_scorer', and any other necessary libraries
+
+# TODO: Initialize the classifier
+clf = None
+
+# TODO: Create the parameters list you wish to tune
+parameters = None
+
+# TODO: Make an fbeta_score scoring object
+scorer = None
+
+# TODO: Perform grid search on the classifier using 'scorer' as the scoring method
+grid_obj = None
+
+# TODO: Fit the grid search object to the training data and find the optimal parameters
+grid_fit = None
+
+# Get the estimator
+best_clf = grid_fit.best_estimator_
+
+# Make predictions using the unoptimized and model
+predictions = (clf.fit(X_train, y_train)).predict(X_test)
+best_predictions = best_clf.predict(X_test)
+
+# Report the before-and-afterscores
+print "Unoptimized model\n------"
+print "Accuracy score on testing data: {:.4f}".format(accuracy_score(y_test, predictions))
+print "F-score on testing data: {:.4f}".format(fbeta_score(y_test, predictions, beta = 0.5))
+print "\nOptimized Model\n------"
+print "Final accuracy score on the testing data: {:.4f}".format(accuracy_score(y_test, best_predictions))
+print "Final F-score on the testing data: {:.4f}".format(fbeta_score(y_test, best_predictions, beta = 0.5))
+
+
+
+# TODO: Import a supervised learning model that has 'feature_importances_'
+
+# TODO: Train the supervised model on the training set 
+model = None
+
+# TODO: Extract the feature importances
+importances = None
+
+# Plot
+vs.feature_plot(importances, X_train, y_train)
+
+
+
+
+# Import functionality for cloning a model
+from sklearn.base import clone
+
+# Reduce the feature space
+X_train_reduced = X_train[X_train.columns.values[(np.argsort(importances)[::-1])[:5]]]
+X_test_reduced = X_test[X_test.columns.values[(np.argsort(importances)[::-1])[:5]]]
+
+# Train on the "best" model found from grid search earlier
+clf = (clone(best_clf)).fit(X_train_reduced, y_train)
+
+# Make new predictions
+reduced_predictions = clf.predict(X_test_reduced)
+
+# Report scores from the final model using both versions of data
+print "Final Model trained on full data\n------"
+print "Accuracy on testing data: {:.4f}".format(accuracy_score(y_test, best_predictions))
+print "F-score on testing data: {:.4f}".format(fbeta_score(y_test, best_predictions, beta = 0.5))
+print "\nFinal Model trained on reduced data\n------"
+print "Accuracy on testing data: {:.4f}".format(accuracy_score(y_test, reduced_predictions))
+print "F-score on testing data: {:.4f}".format(fbeta_score(y_test, reduced_predictions, beta = 0.5))
+
+
+
